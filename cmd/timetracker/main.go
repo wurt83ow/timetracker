eb7939d9dca2c695f44a9c58e1331986befacdbf
wurt83ow <wurt83ow@yandex.ru>
2024-07-14 20:55:18 +0300

    Initial commit

---
+++ cmd/timetracker/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/wurt83ow/timetracker/internal/app"
+)
+
+func main() {
+
+	// Создание корневого контекста с возможностью отмены
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Создание канала для обработки сигналов
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	// Запуск сервера
+	server := app.NewServer(ctx)
+	go func() {
+		// Ожидание сигнала
+		sig := <-signalCh
+		log.Printf("Received signal: %+v", sig)
+
+		// Завершение работы сервера
+		server.Shutdown()
+
+		// Отмена контекста
+		cancel()
+
+	}()
+
+	// Запуск сервера
+	server.Serve()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wurt83ow/timetracker
+
+go 1.22.2
