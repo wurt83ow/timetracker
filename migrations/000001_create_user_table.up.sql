aa39a558b2192c91eb58ff6f275a1a4a70060851
wurt83ow <wurt83ow@yandex.ru>
2024-07-17 14:06:54 +0300

    Removed NotNull restrictions for all fields except key fields in the Users table

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -2,13 +2,13 @@ CREATE TABLE Users (
     id SERIAL PRIMARY KEY,
     passportSerie INTEGER NOT NULL,
     passportNumber INTEGER NOT NULL,
-    surname VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255),
+    name VARCHAR(255),
     patronymic VARCHAR(255),
-    address VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
     default_end_time TIME WITH TIME ZONE,
     timezone VARCHAR(50),    
-    password_hash VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255),
     last_checked_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (passportSerie, passportNumber)
 );
